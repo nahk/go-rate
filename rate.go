5ce3f7d199f0432537fc312c0d1c236880711142
Clément Chéry <clement.chery@blablacar.com>
2017-05-17 14:11:36 +0200

    Adapt rate to handle API limit headers

--- rate.go
+++ rate.go
@@ -1,11 +1,9 @@
 package rate
 
 import (
-	"container/list"
 	"errors"
 	"net/http"
 	"strconv"
-	"sync"
 	"time"
 )
 
@@ -15,19 +13,18 @@ import (
 // granted are steadily increased until a steady throughput equilibrium is
 // reached.
 type RateLimiter struct {
-	Limit   int
-	ResetAt time.Time
-	mtx     sync.Mutex
-	times   list.List
+	limit     int
+	remaining int
+	resetAt   time.Time
 }
 
 // New creates a new rate limiter for the limit and resetAt.
 func New(limit int, resetAt time.Time) *RateLimiter {
 	lim := &RateLimiter{
-		Limit:   limit,
-		ResetAt: resetAt,
+		limit:     limit,
+		remaining: limit,
+		resetAt:   resetAt,
 	}
-	lim.times.Init()
 	return lim
 }
 
@@ -37,44 +34,42 @@ func (r *RateLimiter) Update(header http.Header) error {
 	if err != nil {
 		return errors.New("X-Ratelimit-Reset header is not defined")
 	}
-	r.ResetAt = time.Unix(reset+1, 0)
+	r.resetAt = time.Unix(reset+1, 0)
 
 	remaining, err := strconv.Atoi(header.Get("X-Ratelimit-Remaining"))
 	if err != nil {
 		return errors.New("X-Ratelimit-Remaining header is not defined")
 	}
-	r.Limit = remaining
+	r.remaining = remaining
+
+	limit, err := strconv.Atoi(header.Get("X-Ratelimit-Limit"))
+	if err != nil {
+		return errors.New("X-Ratelimit-Limit header is not defined")
+	}
+	r.limit = limit
 	return nil
 }
 
 // Wait blocks if the rate limit has been reached.  Wait offers no guarantees
 // of fairness for multiple actors if the allowed rate has been temporarily
 // exhausted.
-func (r *RateLimiter) Wait() {
-	for {
-		ok, remaining := r.Try()
-		if ok {
-			break
-		}
-		time.Sleep(remaining)
+func (r *RateLimiter) Wait() (ok bool, pause time.Duration) {
+	ok, pause = r.Try()
+	if !ok {
+		time.Sleep(pause)
 	}
+	return
 }
 
 // Try returns true if under the rate limit, or false if over and the
 // remaining time before the rate limit expires.
 func (r *RateLimiter) Try() (bool, time.Duration) {
-	r.mtx.Lock()
-	defer r.mtx.Unlock()
-	now := time.Now()
-	if l := r.times.Len(); l < r.Limit {
-		r.times.PushBack(now)
+	if r.remaining > 0 {
 		return true, 0
 	}
-	frnt := r.times.Front()
-	if diff := r.ResetAt.Sub(now); diff.Seconds() > 0 {
-		return false, diff
-	}
-	frnt.Value = now
-	r.times.MoveToBack(frnt)
-	return true, 0
+	return false, r.resetAt.Sub(time.Now())
+}
+
+func (r *RateLimiter) Remaining() int {
+	return r.remaining
 }
--- rate_test.go
+++ rate_test.go
@@ -7,46 +7,6 @@ import (
 	"time"
 )
 
-func TestRateLimiter_Wait_noblock(t *testing.T) {
-	start := time.Now()
-	limit := 5
-	interval := start.Add(time.Second * time.Duration(3))
-	limiter := New(limit, interval)
-	for i := 0; i < limit; i++ {
-		limiter.Wait()
-	}
-	if time.Now().Sub(start) >= time.Second * time.Duration(3) {
-		t.Error("The limiter blocked when it shouldn't have")
-	}
-}
-
-func TestRateLimiter_Wait_block(t *testing.T) {
-	start := time.Now()
-	limit := 5
-	interval := start.Add(time.Second * time.Duration(3))
-	limiter := New(limit, interval)
-	for i := 0; i < limit + 1; i++ {
-		limiter.Wait()
-	}
-	if time.Now().Sub(start) < time.Second * time.Duration(3) {
-		t.Error("The limiter didn't block when it should have")
-	}
-}
-
-func TestRateLimiter_Try(t *testing.T) {
-	limit := 5
-	interval := time.Now().Add(time.Second * time.Duration(3))
-	limiter := New(limit, interval)
-	for i := 0; i < limit; i++ {
-		if ok, _ := limiter.Try(); !ok {
-			t.Fatalf("Should have allowed try on attempt %d", i)
-		}
-	}
-	if ok, _ := limiter.Try(); ok {
-		t.Fatal("Should have not allowed try on final attempt")
-	}
-}
-
 func TestRateLimiter_Update(t *testing.T) {
 	start := time.Now()
 	plus1Sec := start.Add(time.Second * time.Duration(1))
@@ -58,26 +18,27 @@ func TestRateLimiter_Update(t *testing.T) {
 
 	header := http.Header{}
 	header.Add("X-Ratelimit-Reset", strconv.FormatInt(plus1Sec.Unix(), 10))
+	header.Add("X-Ratelimit-Limit", strconv.Itoa(10))
 	header.Add("X-Ratelimit-Remaining", strconv.Itoa(10))
 	if err := limiter.Update(header); err != nil {
 		t.Error(err)
 	}
 
-	if limiter.ResetAt.Unix() != plus1Sec.Unix() + 1 {
+	if limiter.resetAt.Unix() != plus1Sec.Unix()+1 {
 		t.Error("The limiter hasn't been updated with the proper reset time")
 	}
-	if limiter.Limit != 10 {
+	if limiter.limit != 10 {
 		t.Error("The limiter hasn't been updated with the proper limit")
 	}
 }
 
-func TestRateLimiter_Wait_update_block(t *testing.T) {
+func TestRateLimiter_Wait(t *testing.T) {
 	start := time.Now()
 	loops := 2
 	wait := 3
 
 	limit := 3
-	limiter := New(limit, start.Add(time.Second * time.Duration(5)))
+	limiter := New(limit, start.Add(time.Second*time.Duration(5)))
 
 	resetAt := start
 	for k := 0; k < loops; k++ {
@@ -87,12 +48,13 @@ func TestRateLimiter_Wait_update_block(t *testing.T) {
 
 			header := http.Header{}
 			header.Add("X-Ratelimit-Reset", strconv.FormatInt(resetAt.Unix(), 10))
-			header.Add("X-Ratelimit-Remaining", strconv.Itoa(limit - i))
+			header.Add("X-Ratelimit-Limit", strconv.Itoa(limit))
+			header.Add("X-Ratelimit-Remaining", strconv.Itoa(limit-i))
 			limiter.Update(header)
 		}
 	}
 
-	if time.Now().Sub(start) < time.Second * time.Duration(wait * loops) {
+	if time.Now().Sub(start) < time.Second*time.Duration(wait*(loops-1)) {
 		t.Error("The limiter didn't block when it should have")
 	}
 }
